09c2d5b4b3e4fd2ecaa4f6a56c328fe5730cf7dc
rishi-potdukhe12 <108914522+rishi-potdukhe12@users.noreply.github.com>
2022-07-14 09:20:30 +0530

    Update sample.java

--- src/main/sample.java
+++ src/main/sample.java
@@ -2,6 +2,7 @@ package org.jenkins;
 public class sample{
 	public static void main( String[] args) {
 		System.out.println("Hello From Maven");
+		System.out.println("Good Morning From Maven");
 		
 	}
 }
